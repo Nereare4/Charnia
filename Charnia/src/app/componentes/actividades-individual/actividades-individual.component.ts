9a1b203cc26fda1096ba16c2d3e2fc1d1169de9d
Nereare4 <94044761+Nereare4@users.noreply.github.com>
2022-04-07 21:04:42 +0200

    Actividad individual
    Funciona botones + opacidad

--- Charnia/src/app/componentes/actividades-individual/actividades-individual.component.ts
+++ Charnia/src/app/componentes/actividades-individual/actividades-individual.component.ts
@@ -10,15 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ActividadesIndividualComponent implements OnInit {
 
-  individual:any;
+  individual: any;
   nombre: any;
+  ruta: string[]=[];
+  ok: boolean = true;
 
   constructor(private conexion: ConexionService, private route: ActivatedRoute) {
     this.conexion.listaIndividual().subscribe(indiv => {
       this.individual = indiv;
+      for (let i = 0; i < indiv.length; i++) {
+        this.ruta.push(indiv[i].nombre);
+      }
     })
   }
   ngOnInit() {
     this.nombre = this.route.snapshot.paramMap.get("nombre");
+    this.ruta;
   }
+  
+
 }
