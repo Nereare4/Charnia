388de460e604f9ce2111cde83fee8eca08c0aeae
Nereare4 <94044761+Nereare4@users.noreply.github.com>
2022-06-15 20:04:12 +0200

    Correcciones responsive

--- Charnia/src/environments/environment.prod.ts
+++ Charnia/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDKQhl1MTAhgzdjA7kMUQx8kkaoQd8k2Gs",
+  authDomain: "charnia-b944f.firebaseapp.com",
+  projectId: "charnia-b944f",
+  storageBucket: "charnia-b944f.appspot.com",
+  messagingSenderId: "92391030017",
+  appId: "1:92391030017:web:cb284c038a463fd6866f00",
+  measurementId: "G-H7M1HXMH4E"
+  },
 };
